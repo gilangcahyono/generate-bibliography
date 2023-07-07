5e81e6740fc2ce140d52f684b2235b4ee322fdb3
Gilang Cahyono <gilcah2207@gmail.com>
2023-07-07 18:10:33 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+export default function App() {
+  return (
+    <>
+      <h1>Testing Deploy React Vite to Github Pages</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt facere
+        laborum ipsum minus aperiam est molestias, in ullam at voluptatum optio
+        illum a atque pariatur dolorem, ex, mollitia debitis quos! Repellat
+        facere iusto ab harum quis! Consequuntur provident consequatur, eos nam
+        eius, ad, laborum voluptas tempora magni a magnam nemo?
+      </p>
+    </>
+  )
+}
